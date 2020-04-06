ccbfd590e6ec56a9c41d18655e85d9ae23c03d00
Jérémy Marin <31868712+c0deless@users.noreply.github.com>
2020-04-06 18:27:58 +0200

    Create downloadandrename.py

---
+++ downloadandrename.py
@@ -0,0 +1,12 @@
+import shutil
+import urllib.request as request
+from contextlib import closing
+
+def download(filename):
+    with closing(request.urlopen("ftp://username:password@ftp.example.com/"+filename)) as r:
+        with open('C:/MyPath/recievingfolder/'+filename,'wb') as f:
+            shutil.copyfileobj(r, f)
+
+files = ["file.mp3", "fild1.html"]
+for file in files:
+    download(file)
