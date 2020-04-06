66f72ce51435636b0a2e9b5c0daff1800f0879c5
Jérémy Marin <31868712+c0deless@users.noreply.github.com>
2020-04-06 18:29:19 +0200

    Update downloadandrename.py

--- downloadandrename.py
+++ downloadandrename.py
@@ -4,9 +4,9 @@ from contextlib import closing
 
 def download(filename):
     with closing(request.urlopen("ftp://username:password@ftp.example.com/"+filename)) as r:
-        with open('C:/MyPath/recievingfolder/'+filename,'wb') as f:
+       with open('C:/MyPath/recievingfolder/'+filename[1],'wb') as f:
             shutil.copyfileobj(r, f)
 
-files = ["file.mp3", "fild1.html"]
+files = [("file.mp3","new_name_1.mp3"), ("filed.html","new_name_1.html")]
 for file in files:
     download(file)
